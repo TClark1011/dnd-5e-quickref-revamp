ebf64f47ccfc3a6245e2e1cbfb07f169177441a8
Thomas Clark <thomas.clark@bigtincan.com>
2022-03-13 21:40:01 +1100

    added metadata

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,12 +3,23 @@
 	import '../styles/_fonts.less';
 	import data from '../data';
 	import { generateSectionColorCss } from '../logic';
+	import { page } from '$app/stores';
 
 	const { lightModeStyles, darkModeStyles } = generateSectionColorCss(data);
+
+	const title = '5e Quick Reference';
 </script>
 
 {@html lightModeStyles}
 {@html darkModeStyles}
+
+<svelte:head>
+	<title>{title}</title>
+	<meta property="og:title" content={title} />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content={$page.url.toString()} />
+	<meta property="og:image" content="thumbnail.png" />
+</svelte:head>
 <div id="root">
 	<slot />
 </div>
